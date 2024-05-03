25e6247cb057cef87b4c133e741f8dada5f89459
developeralamgirbd <developer.alamgirbdg@gmail.com>
2024-05-03 21:04:35 +0600

    [Done] env example

--- src/APIs/axiosInstance.js
+++ src/APIs/axiosInstance.js
@@ -2,9 +2,9 @@ import axios from "axios";
 // import { getToken } from "./sessionStorage";
 export const AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
+ /* headers: {
     Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
-  },
+  },*/
   // headers: {
   //   Authorization: `Bearer ${getToken()}`,
   // },
